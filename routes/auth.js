4c634e9c8d5bdc88bafc9a942b1c41fb71bdba27
AmirUhmmer <jazmirjulio@gmail.com>
2024-11-11 16:12:05 +0100

    convert routes to esm

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,44 @@
-const express = require('express');
-const { getAuthorizationUrl, authCallbackMiddleware, authRefreshMiddleware, getUserProfile } = require('../services/aps.js');
+// const express = require('express');
+// const { getAuthorizationUrl, authCallbackMiddleware, authRefreshMiddleware, getUserProfile } = require('../services/aps.js');
+
+// let router = express.Router();
+
+// router.get('/api/auth/login', function (req, res) {
+//     res.redirect(getAuthorizationUrl());
+// });
+
+// router.get('/api/auth/logout', function (req, res) {
+//     req.session = null;
+//     res.redirect('/');
+// });
+
+// router.get('/api/auth/callback', authCallbackMiddleware, function (req, res) {
+//     res.redirect('/');
+// });
+
+// router.get('/api/auth/token', authRefreshMiddleware, function (req, res) {
+//     res.json(req.publicOAuthToken);
+// });
+
+// router.get('/api/auth/profile', authRefreshMiddleware, async function (req, res, next) {
+//     try {
+//         const profile = await getUserProfile(req.internalOAuthToken.access_token);
+//         res.json({ name: `${profile.name}` });
+//     } catch (err) {
+//         next(err);
+//     }
+// });
+
+// module.exports = router;
+
+
+
+
+
+// converted to esm 
+
+import express from 'express';
+import { getAuthorizationUrl, authCallbackMiddleware, authRefreshMiddleware, getUserProfile } from '../services/aps.js';
 
 let router = express.Router();
 
@@ -29,4 +68,6 @@ router.get('/api/auth/profile', authRefreshMiddleware, async function (req, res,
     }
 });
 
-module.exports = router;
+// Export the router as the default export
+export default router;
+
--- routes/hubs.js
+++ routes/hubs.js
@@ -1,11 +1,70 @@
-const express = require('express');
-const { authRefreshMiddleware, getHubs, getProjects, getProjectContents, getItemVersions } = require('../services/aps.js');
+// const express = require('express');
+// const { authRefreshMiddleware, getHubs, getProjects, getProjectContents, getItemVersions } = require('../services/aps.js');
 
-let router = express.Router();
+// let router = express.Router();
 
+// router.use('/api/hubs', authRefreshMiddleware);
+
+// router.get('/api/hubs', async function (req, res, next) {
+//     try {
+//         const hubs = await getHubs(req.internalOAuthToken.access_token);
+//         res.json(hubs);
+//     } catch (err) {
+//         next(err);
+//     }
+// });
+
+// router.get('/api/hubs/:hub_id/projects', async function (req, res, next) {
+//     try {
+//         const projects = await getProjects(req.params.hub_id, req.internalOAuthToken.access_token);
+//         res.json(projects);
+//     } catch (err) {
+//         next(err);
+//     }
+// });
+
+// router.get('/api/hubs/:hub_id/projects/:project_id/contents', async function (req, res, next) {
+//     try {
+//         const contents = await getProjectContents(req.params.hub_id, req.params.project_id, req.query.folder_id, req.internalOAuthToken.access_token);
+//         res.json(contents);
+//     } catch (err) {
+//         next(err);
+//     }
+// });
+
+// router.get('/api/hubs/:hub_id/projects/:project_id/contents/:item_id/versions', async function (req, res, next) {
+//     try {
+//         const versions = await getItemVersions(req.params.project_id, req.params.item_id, req.internalOAuthToken.access_token);
+//         res.json(versions);
+//     } catch (err) {
+//         next(err);
+//     }
+// });
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+//esm
+
+import express from 'express';
+import { authRefreshMiddleware, getHubs, getProjects, getProjectContents, getItemVersions } from '../services/aps.js';
+
+const router = express.Router();
+
+// Apply authRefreshMiddleware to the /api/hubs route
 router.use('/api/hubs', authRefreshMiddleware);
 
-router.get('/api/hubs', async function (req, res, next) {
+// Get hubs
+router.get('/api/hubs', async (req, res, next) => {
     try {
         const hubs = await getHubs(req.internalOAuthToken.access_token);
         res.json(hubs);
@@ -14,7 +73,8 @@ router.get('/api/hubs', async function (req, res, next) {
     }
 });
 
-router.get('/api/hubs/:hub_id/projects', async function (req, res, next) {
+// Get projects by hub ID
+router.get('/api/hubs/:hub_id/projects', async (req, res, next) => {
     try {
         const projects = await getProjects(req.params.hub_id, req.internalOAuthToken.access_token);
         res.json(projects);
@@ -23,7 +83,8 @@ router.get('/api/hubs/:hub_id/projects', async function (req, res, next) {
     }
 });
 
-router.get('/api/hubs/:hub_id/projects/:project_id/contents', async function (req, res, next) {
+// Get project contents by project ID
+router.get('/api/hubs/:hub_id/projects/:project_id/contents', async (req, res, next) => {
     try {
         const contents = await getProjectContents(req.params.hub_id, req.params.project_id, req.query.folder_id, req.internalOAuthToken.access_token);
         res.json(contents);
@@ -32,7 +93,8 @@ router.get('/api/hubs/:hub_id/projects/:project_id/contents', async function (re
     }
 });
 
-router.get('/api/hubs/:hub_id/projects/:project_id/contents/:item_id/versions', async function (req, res, next) {
+// Get item versions by project ID and item ID
+router.get('/api/hubs/:hub_id/projects/:project_id/contents/:item_id/versions', async (req, res, next) => {
     try {
         const versions = await getItemVersions(req.params.project_id, req.params.item_id, req.internalOAuthToken.access_token);
         res.json(versions);
@@ -41,4 +103,5 @@ router.get('/api/hubs/:hub_id/projects/:project_id/contents/:item_id/versions', 
     }
 });
 
-module.exports = router;
+// Export the router as the default export
+export default router;
